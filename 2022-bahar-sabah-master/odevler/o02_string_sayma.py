1c392eda12ac07ae22eb783de7b71ef75bd46ec3
Salih Algul <salihalgul@gmail.com>
2022-06-06 21:46:37 +0300

    new updates

--- 2022-bahar-sabah-master/odevler/o02_string_sayma.py
+++ 2022-bahar-sabah-master/odevler/o02_string_sayma.py
@@ -29,3 +29,14 @@ Ne sen bunun farkındasın ne polis farkında
 Ben bir ceviz ağacıyım Gülhane Parkı’nda
 Ne sen bunun farkındasın ne de polis farkında
 """
+
+
+sesli_harfler = ['a', 'e', 'ı', 'i', 'o', 'ö', 'u', 'ü']
+ornek_string = "Başım köpük köpük bulut İçim dışım deniz Ben bir ceviz ağacıyım Gülhane Parkı’nda Budak budak, " \
+        "şerham şerham ihtiyar bir ceviz Ne sen bunun farkındasın ne polis farkında Ben bir ceviz ağacıyım" \
+        " Gülhane Parkı’nda Ne sen bunun farkındasın ne de polis farkında"
+ornek_sonucu = []
+for i in ornek_string:
+        if i == sesli_harfler:
+                ornek_sonucu.count(i)
+print(ornek_sonucu)
